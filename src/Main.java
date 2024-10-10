4f72080aed1268dad44c6f232565c43260240449
Nikita Rubtsov <118419926+kad1rka@users.noreply.github.com>
2024-10-10 20:54:42 +0300

    modeling of the bank's operation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import com.sun.tools.jconsole.JConsoleContext;
 
 import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -16,5 +18,54 @@ public class Main {
         System.out.println("Баланс после снятия: " + account.getBalance());
         account.deposit(50000);
         System.out.println("Баланс после пополнения: " + account.getBalance());
+
+        Account[] accounts = new Account[10];
+        for (int i = 0; i < 10; i++) {
+            accounts[i] = new Account(i, 10000);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        while (true) {
+            System.out.printf("\nВведите ID: ");
+            int id = scanner.nextInt();
+            if (id >= 0 && id < 10) {
+                do {
+                    Account selectAccount = accounts[id];
+                    System.out.println("\nВыберите действие:");
+                    System.out.println("1. Проверить баланс счета");
+                    System.out.println("2. Снять со счета");
+                    System.out.println("3. Положить на счет");
+                    System.out.println("4. Выйти");
+                    System.out.printf("Введите пункт меню: \n");
+                    choice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (choice) {
+                        case 1:
+                            System.out.println("\nБаланс счета: "+selectAccount.getBalance());
+                            break;
+                        case 2:
+                            System.out.printf("\nВведите сумму для снятия: ");
+                            double amountForWithdraw = scanner.nextDouble();
+                            selectAccount.withdraw(amountForWithdraw);
+                            break;
+                        case 3:
+                            System.out.printf("\nВведите сумму для поплнения: ");
+                            double amountForDeposit = scanner.nextDouble();
+                            selectAccount.withdraw(amountForDeposit);
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            System.out.println("\nНеверный выбор. Пожалуйста, попробуйте снова.");
+
+                    }
+                } while (choice != 4);
+            } else {
+                System.out.println("\nНеверный выбор ID. Пожалуйста, попробуйте снова");
+            }
+        }
+
     }
 }
